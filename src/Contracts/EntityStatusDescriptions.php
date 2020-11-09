9080b2d7e9facfbb8ad603b07a878703755399ec
simon <sdiaz.sz@gmail.com>
2020-11-09 19:21:14 -0300

    [Update] remove EntityStatusDescriptions::descriptions and EntityStatusDescriptions::getDescription parameter type

--- src/Contracts/EntityStatusDescriptions.php
+++ src/Contracts/EntityStatusDescriptions.php
@@ -3,7 +3,5 @@
 namespace Larangular\EntityStatus\Contracts;
 
 interface EntityStatusDescriptions {
-    public function descriptions(): array;
-
-    public function getDescription(int $index): ?string;
+    public function getDescription($key): ?string;
 }
