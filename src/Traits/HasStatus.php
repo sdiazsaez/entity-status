4cd34b20fe7050d0a65ca62978fe5db8135333c2
simon <sdiaz.sz@gmail.com>
2022-12-26 22:59:38 -0300

    [Update] HasStatus trait add support for key

--- src/Traits/HasStatus.php
+++ src/Traits/HasStatus.php
@@ -3,22 +3,27 @@
 namespace Larangular\EntityStatus\Traits;
 
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Support\Str;
 use Larangular\EntityStatus\Contracts\EntityStatusDescriptions;
 use Larangular\EntityStatus\Models\EntityStatus;
 
 trait HasStatus {
 
-    abstract public function entityStatusDescriptions(): EntityStatusDescriptions;
+    abstract public function entityStatusDescriptions(string $key): EntityStatusDescriptions;
 
     public function status(): MorphOne {
         return $this->morphOne(EntityStatus::class, 'entity');
     }
 
-    public function setEntityStatus($value) {
-        $s = $this->status()->firstOrNew([]);
-        $s->status = $value;
-        $s->save();
-        return $this;
+    public function entityStatus(string $key, $value = null, $message = null) {
+        $w = ['key' => Str::slug($key)];
+        return is_null($value)
+            ? $this->status()->where($w)->first()
+            : $this->status()->updateOrCreate($w, ['status' => $value, 'message' => $message]);
+    }
+
+    public function setEntityStatus(string $key, $value) {
+        return $this->entityStatus($key, $value);
     }
 
 }
