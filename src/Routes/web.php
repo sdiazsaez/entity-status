add2400a7ee183a0d34c40be9ea88875272fb5a2
simon <sdiaz.sz@gmail.com>
2023-02-21 18:15:25 -0300

    fix...

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
-use Larangular\EntityStatus\Http\Controllers\EntityGateway;
-
 Route::group([
     'prefix'     => 'api/entity-status/',
     'middleware' => 'api',
-    'namespace'  => 'Larangular\EntityStatus\Http\Controllers',
+    'namespace'  => '\Larangular\EntityStatus\Http\Controllers',
     'as'         => 'larangular.api.entity-status.',
 ], static function () {
-    Route::resource('status', EntityGateway::class);
+    Route::resource('status', 'EntityGateway');
 });
